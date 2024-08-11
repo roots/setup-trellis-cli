9cd7850a2a514eba71880879babe14c10c79cf02
Ben Word <ben@benword.com>
2024-08-11 11:58:46 -0500

    👀

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-/******/ (() => { // webpackBootstrap
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7799:
@@ -76251,3 +76251,4 @@ run();
 module.exports = __webpack_exports__;
 /******/ })()
 ;
+//# sourceMappingURL=index.js.map
